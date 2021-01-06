f888c17a09c6d1111c9e8dedfa0925597f1812fb
sunxin <gzsunxin@rd.netease.com>
2021-01-06 16:53:34 +0800

    init

--- Sources/PackageTest/PackageTest.swift
+++ Sources/PackageTest/PackageTest.swift
@@ -1,3 +1,19 @@
 struct PackageTest {
     var text = "Hello, World!"
 }
+
+#if os(Linux)
+
+// do something on Linux
+
+#elseif os(macOS)
+
+// do something on macOS
+
+#endif
+
+#if canImport(UIKit)
+
+// do something on platforms where UIKit is available
+
+#endif
